c5f467cf700d243d5c6af825bf7862f190366943
KajalGupta2003 <kajalguptak2003@gmail.com>
2026-07-23 14:46:50 +0530

    prepare backend for deployment

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,12 @@ from config import MONGODB_URI
 
 client = MongoClient(MONGODB_URI)
 
+try:
+    client.admin.command("ping")
+    print(" MongoDB Connected Successfully")
+except Exception as e:
+    print("MongoDB Connection Failed:", e)
+
 db = client["interviewiq"]
 interviews_collection = db["interviews"]
 
-print(" MongoDB Connected Successfully")
