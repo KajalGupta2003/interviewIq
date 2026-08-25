1c01496f29b0c1a697cacbec13e9c88e57dd9e5d
KajalGupta2003 <kajalguptak2003@gmail.com>
2026-08-25 14:41:50 +0530

    problem in cors

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["*"],
-    allow_origins=["https://interviewiq-tan.vercel.app"],
+    allow_origins=[ FRONTEND_URL,
+        "http://localhost:5173",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
