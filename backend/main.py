3d1704fc22dd54f99b177a4452f45435db354fad
KajalGupta2003 <kajalguptak2003@gmail.com>
2026-08-25 13:07:18 +0530

    cors

--- backend/main.py
+++ backend/main.py
@@ -44,7 +44,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     # allow_origins=["*"],
-    allow_origins=[FRONTEND_URL],
+    allow_origins=["https://interviewiq-tan.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
