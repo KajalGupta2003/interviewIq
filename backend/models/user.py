390f079fb46b61fec8d3557c81b8132b43f96c13
KajalGupta2003 <kajalguptak2003@gmail.com>
2026-08-25 11:29:16 +0530

    fix user schema and document helpers

--- backend/models/user.py
+++ backend/models/user.py
@@ -6,3 +6,19 @@ class User(BaseModel):
     name: str
     email: EmailStr
     photo: Optional[str] = None
+
+
+def create_user_doc(user_data: dict):
+    return {
+        "name": user_data["name"],
+        "email": user_data["email"],
+        "photo": user_data.get("photo") or user_data.get("picture"),
+    }
+
+
+def user_schema(user: dict):
+    return {
+        "name": user.get("name"),
+        "email": user.get("email"),
+        "photo": user.get("photo"),
+    }
